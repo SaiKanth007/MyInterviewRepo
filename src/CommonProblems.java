426f5bd54b80c2ed1aa9fbec99a811d238badde7
gsaikan <saikanth.gunda@optum.com>
2019-02-02 10:44:49 +0530

    Organizing the changes

--- src/CommonProblems.java
+++ src/CommonProblems.java
@@ -268,6 +268,47 @@ public class CommonProblems {
 		return "";
 	}
 
+	  public static String covertNumberToRomans(Integer Number) {
+	        return "";
+	    }
+
+	    public static Integer covertRomansToNumber(String romanNumber) {
+	        if (romanNumber == null || romanNumber == " ") {
+	            return 0;
+	        }
+	        final char[] array = romanNumber.toCharArray();
+	        final int length = array.length;
+	        final Map<String, Integer> romantToNumberMap = MyJavaUtils.getRomansToNumberMap();
+	        if (length == 1) {
+	            return romantToNumberMap.get(String.valueOf(array[0]));
+	        }
+	        Integer sum = romantToNumberMap.get(String.valueOf(array[length - 1]));
+	        for (int i = length - 2; i >= 0; i--) {
+	            if (romantToNumberMap.get(String.valueOf(array[i])) < romantToNumberMap.get(String.valueOf(array[i + 1]))) {
+	                sum = sum - romantToNumberMap.get(String.valueOf(array[i]));
+	            } else {
+	                sum = romantToNumberMap.get(String.valueOf(array[i])) + sum;
+
+	            }
+
+	        }
+	        return sum;
+
+	    }
+
+	    public static boolean checkIfPrime(Integer number) {
+	        for (int index = 2; index < Math.sqrt(number); index++) {
+	            if (number % index == 0) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+
+	    public static int choclateWrapperProblem(int money, int price, int wrapper) {
+	        return 0;
+	    }
+
 	/**
 	 * length of maximum sub array with equal ones and zeros
 	 */
--- src/Utilities/Tesing.java
+++ src/Utilities/Tesing.java
@@ -5,6 +5,12 @@ public class Tesing {
 		// stackOverFlowMethod(); // causes stack over flow error
 		//remove all characters other than alphabets from the given string
 		System.out.println("Sai".replaceAll("[^a-bA-Z]",""));
+		
+		  System.out.println("Hello World");
+	       String string = "string";
+	       String[] abc = "I am good".split(" ");
+	       System.out.println(abc.length);
+	       System.out.println(string.matches("[^0-9]+"));
 
 		outOfMemoryMethod();
 		
